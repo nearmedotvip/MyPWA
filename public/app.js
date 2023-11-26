2844fe82e7f4d747b6a1b44ded10191fd564a16d
Christopher Caston <chris@near-me.vip>
2023-11-26 08:58:22 +0800

    Added geolocation code.

--- public/app.js
+++ public/app.js
@@ -1,3 +1,28 @@
 // Your JavaScript code goes here
 console.log("PWA is running!");
+// app.js
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("PWA is running!");
+
+    // Check if geolocation is supported
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords;
+
+            // Call a function to get the suburb based on latitude and longitude
+            getSuburbFromCoordinates(latitude, longitude);
+        }, (error) => {
+            console.error("Error getting location:", error.message);
+        });
+    } else {
+        console.error("Geolocation is not supported in this browser");
+    }
+});
+
+function getSuburbFromCoordinates(latitude, longitude) {
+    // You can use a geocoding service or your own logic to get the suburb
+    // For simplicity, let's just display the coordinates for now
+    const suburbElement = document.getElementById("suburb");
+    suburbElement.textContent = `Latitude: ${latitude}, Longitude: ${longitude}`;
+}
 
